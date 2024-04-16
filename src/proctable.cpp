5cbb976cf5a5e1469efe4762e7a5a732bcccca31
Pablo Barciela <scow@riseup.net>
2024-04-16 21:05:43 +0200

    rename total_bytes_esticafe -> total_bytes_estimate

--- src/proctable.cpp
+++ src/proctable.cpp
@@ -652,7 +652,7 @@ get_process_memory_info(ProcInfo *info)
     info->memres    = procmem.resident;
     info->memshared = procmem.share;
 
-    info->memxserver = xresources.total_bytes_esticafe;
+    info->memxserver = xresources.total_bytes_estimate;
 
     get_process_memory_writable(info);
 
